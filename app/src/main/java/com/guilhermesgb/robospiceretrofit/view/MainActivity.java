615770323fbca5ba6e805659edf7addb23b9e46f
Guilherme Baptista <guisgb13@gmail.com>
2015-08-17 16:38:17 -0300

    Experimenting with NavigationDrawers (DrawerLayout + NavigationViews)

--- app/src/main/java/com/guilhermesgb/robospiceretrofit/view/MainActivity.java
+++ app/src/main/java/com/guilhermesgb/robospiceretrofit/view/MainActivity.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,8 +20,11 @@ import com.hannesdorfmann.mosby.MosbyActivity;
 
 import butterknife.Bind;
 
-public class MainActivity extends MosbyActivity {
+public class MainActivity extends MosbyActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    @Bind(R.id.drawerLayout) DrawerLayout drawerLayout;
+    @Bind(R.id.navigationViewLeft) NavigationView navigationViewLeft;
+    @Bind(R.id.navigationViewRight) NavigationView navigationViewRight;
     @Bind(R.id.coordinatorLayout) CoordinatorLayout coordinatorLayout;
     @Bind(R.id.collapsingToolbarLayout) CollapsingToolbarLayout collapsingToolbarLayout;
     @Bind(R.id.appToolbar) Toolbar toolbar;
@@ -29,6 +34,8 @@ public class MainActivity extends MosbyActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        navigationViewLeft.setNavigationItemSelectedListener(this);
+        navigationViewRight.setNavigationItemSelectedListener(this);
         toolbar.setNavigationIcon(R.drawable.notification_icon);
         setSupportActionBar(toolbar);
         collapsingToolbarLayout.setTitle(getString(R.string.app_name));
@@ -56,7 +63,7 @@ public class MainActivity extends MosbyActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -72,6 +79,18 @@ public class MainActivity extends MosbyActivity {
 
     public CoordinatorLayout getCoordinatorLayout() {
         return coordinatorLayout;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.navigation_sub_item_2_1) {
+            drawerLayout.closeDrawer(navigationViewLeft);
+            drawerLayout.closeDrawer(navigationViewRight);
+            Snackbar.make(coordinatorLayout, "NAV 1.2 Clicked", Snackbar.LENGTH_SHORT).show();
+            return true;
+        }
+        return false;
     }
 
 }
